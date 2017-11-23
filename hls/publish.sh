287acd10b086798680e8550ddf58c00d99eafac3
ZhangFeng <zhangfeng@jk.cn>
2017-11-23 23:13:44 +0800

    Using dockerized ffmpeg

--- hls/publish.sh
+++ hls/publish.sh
@@ -1,1 +1,2 @@
-ffmpeg -re -i ~/Downloads/ts.mp4 -bsf:v h264_mp4toannexb -c copy -f mpegts http://streaming:8000/publish/timeseries
+# TODO replace loving_poitras with container name of the nginx
+docker run -itd -v ~/Downloads:/data/video --link loving_poitras:streaming jrottenberg/ffmpeg:3.3-alpine -re -i /data/video/ts.mp4 -bsf:v h264_mp4toannexb -c copy -f mpegts http://streaming/publish/timeseries
